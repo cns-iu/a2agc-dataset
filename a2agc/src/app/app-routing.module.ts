b7852c15f1f2ce43c5a1d9a1bd3086168baaaf0f
Adam Phillips <adjp2013@gmail.com>
2020-10-28 11:05:54 -0400

    geomap moved over to new site

--- a2agc/src/app/app-routing.module.ts
+++ a2agc/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   {
     path: 'geomap',
     loadChildren: () => import('./pages/geomap/geomap.module').then(m => m.GeomapModule)
-  }
+  },
+  { path: 'vis1-geomap-of-opioid-deaths', loadChildren: () => import('./pages/vis1-geomap-of-opioid-deaths/vis1-geomap-of-opioid-deaths.module').then(m => m.Vis1GeomapOfOpioidDeathsModule) }
 ];
 
 @NgModule({
