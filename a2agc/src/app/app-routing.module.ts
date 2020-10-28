e3fe2dc29492e34750f13d6329dfcc96c0b84163
Adam Phillips <adjp2013@gmail.com>
2020-10-28 12:25:50 -0400

    added heatmap vis to new site

--- a2agc/src/app/app-routing.module.ts
+++ a2agc/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   {
     path: 'geomap',
     loadChildren: () => import('./pages/geomap/geomap.module').then(m => m.GeomapModule)
-  }
+  },
+  { path: 'vis3-heatmap-of-accidental-overdoses', loadChildren: () => import('./pages/vis3-heatmap-of-accidental-overdoses/vis3-heatmap-of-accidental-overdoses.module').then(m => m.Vis3HeatmapOfAccidentalOverdosesModule) }
 ];
 
 @NgModule({
