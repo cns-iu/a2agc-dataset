68404b136af9b966ae6f10163502da2305dae5a9
Edward Lu <edlu@iu.edu>
2021-05-26 08:00:27 -0400

    Upgrade specs to vega-lite 5

--- website/src/app/build-info.ts
+++ website/src/app/build-info.ts
@@ -1,5 +1,5 @@
 export const buildInfo = {
   version: '1.0.0',
-  lastCommitDate: new Date(1603996553000),
-  buildDate: new Date(1603996944707)
+  lastCommitDate: new Date(1611167362000),
+  buildDate: new Date(1622021086252)
 };
