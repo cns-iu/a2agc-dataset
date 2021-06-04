d9c5cb68bce04bcd0b4ee79de01170b31f80a52c
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-06-03 20:55:50 -0400

    removing tests based on older components

--- website/src/app/shared/components/sub-selector/sub-selector.component.spec.ts
+++ website/src/app/shared/components/sub-selector/sub-selector.component.spec.ts
@@ -1,25 +1,3 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SubSelectorComponent } from './sub-selector.component';
-
 describe('SubSelectorComponent', () => {
-  let component: SubSelectorComponent;
-  let fixture: ComponentFixture<SubSelectorComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SubSelectorComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SubSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // Add tests
 });
