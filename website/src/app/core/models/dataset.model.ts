4ffdfea06263d1b513369a892bca24c70f50c19a
axdanbol <daniel_bolin@yahoo.com>
2021-08-12 09:56:07 -0400

    Refactored dataset models

--- website/src/app/core/models/dataset.model.ts
+++ website/src/app/core/models/dataset.model.ts
@@ -1,38 +1,19 @@
-import { VisualizationSpec } from 'vega-embed';
-import { SummaryDistData } from './table-data.model';
+import { Distribution } from './distribution.model';
+
 
 export interface Dataset {
-  dataset: string;
-  dataVariables: string[];
-  description?: string;
-  subLabel?: string;
-  subDataVariables?: string[];
-  specs: Record<string, VisualizationSpec>;
-  columns: DatasetColumns;
-}
-
-export interface DatasetColumns {
-  [key: string]: DatasetColumn;
-}
-
-export interface DatasetSummary {
-  label: string;
-  value: string;
-}
-
-export interface DatasetColumn {
-  distData: string | VisualizationSpec | SummaryDistData;
-  distType: string;
-  nonNullCount: number;
   name: string;
-  percentMissing: number;
-  remarks: string;
-  type: string;
+  description: string;
+  variables: string[];
 }
 
-export const EMPTY_DATASET: Dataset = {
-  dataset: '',
-  dataVariables: [],
-  specs: {},
-  columns: {}
-};
+export interface DatasetVariable {
+  dataset: string;
+  name: string;
+  description: string;
+
+  type: string;
+  nonNullCount: number;
+  percentMissing: number;
+  distribution: Distribution;
+}
