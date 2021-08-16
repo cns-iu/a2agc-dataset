df05da0108a44e434c238d0ec9354d2936d1a4ce
axdanbol <daniel_bolin@yahoo.com>
2021-08-16 13:17:02 -0400

    Initial refactor of data distribution component

--- website/src/app/pages/data-distributions/data-distributions.component.ts
+++ website/src/app/pages/data-distributions/data-distributions.component.ts
@@ -1,5 +1,11 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
-import { DataDistributionsState } from 'src/app/core/state/data-distribution/data-distribution.state';
+import { Observable, ObservableInput, ReplaySubject } from 'rxjs';
+import { switchAll } from 'rxjs/operators';
+import { VisualizationSpec } from 'vega-embed';
+
+import { Dataset, DatasetVariable } from '../../core/models/dataset.model';
+import { DatasetVariablesState } from '../../core/state/data/dataset-variables.state';
+import { DatasetsState } from '../../core/state/data/datasets.state';
 
 
 @Component({
@@ -10,12 +16,42 @@ import { DataDistributionsState } from 'src/app/core/state/data-distribution/dat
 })
 export class DataDistributionsComponent {
   /** HTML class name */
-  @HostBinding('class') readonly clsName = 'data-schema-browser';
+  @HostBinding('class') readonly clsName = 'agc-data-distributions';
 
-  constructor(readonly data: DataDistributionsState) {}
+  readonly datasets$: Observable<Dataset[]>;
+  readonly variables$: Observable<DatasetVariable[]>;
+  readonly subLabel$: Observable<string>;
+  readonly subVariables$: Observable<DatasetVariable[]>;
 
-  log<T>(data: T): T {
-    console.log(data);
-    return data;
+  selectedDataset?: Dataset;
+  selectedVariable?: DatasetVariable;
+  specs: VisualizationSpec[] = [];
+
+  private readonly variableObservables$ = new ReplaySubject<ObservableInput<DatasetVariable[]>>(1);
+  private readonly subVariableObservables$ = new ReplaySubject<ObservableInput<DatasetVariable[]>>(1);
+
+  constructor(
+    datasetsState: DatasetsState,
+    private readonly variablesState: DatasetVariablesState
+  ) {
+    this.datasets$ = datasetsState.entitiesArray$;
+    this.subLabel$ = variablesState.subLabel$;
+    this.variables$ = this.variableObservables$.pipe(switchAll());
+    this.subVariables$ = this.subVariableObservables$.pipe(switchAll());
+  }
+
+  setSelectedDataset(dataset: Dataset): void {
+    if (dataset !== this.selectedDataset) {
+      this.selectedDataset = dataset;
+      this.selectedVariable = undefined;
+      this.variableObservables$.next(this.variablesState.getVariables(dataset));
+      this.subVariableObservables$.next(this.variablesState.getSubVariables(dataset));
+    }
+  }
+
+  setSelectedVariable(variable: DatasetVariable): void {
+    if (variable !== this.selectedVariable && this.selectedDataset !== undefined) {
+      this.selectedVariable = variable;
+    }
   }
 }
