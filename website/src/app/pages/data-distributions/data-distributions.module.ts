3122ddb7356e976ad54f9228cc1ac2a9b1f940ac
axdanbol <daniel_bolin@yahoo.com>
2021-08-16 13:16:15 -0400

    Updated data distribution page module deps

--- website/src/app/pages/data-distributions/data-distributions.module.ts
+++ website/src/app/pages/data-distributions/data-distributions.module.ts
@@ -1,25 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NgxVegaModule } from 'ngx-vega';
 
+import { LazyVisualizationModule } from '../../shared/components/lazy-visualization/lazy-visualization.module';
 import { TableDataSelectorModule } from '../../shared/components/table-data-selector/table-data-selector.module';
-import { DatasetSummaryModule } from './../../shared/components/dataset-summary/dataset-summary.module';
 import { DataDistributionsRoutingModule } from './data-distributions-routing.module';
 import { DataDistributionsComponent } from './data-distributions.component';
-import { StateModule } from './state/state.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
 
-    NgxVegaModule,
-
-    DatasetSummaryModule,
+    LazyVisualizationModule,
     TableDataSelectorModule,
 
     DataDistributionsRoutingModule,
-    StateModule
   ],
   declarations: [DataDistributionsComponent],
   exports: [DataDistributionsComponent]
