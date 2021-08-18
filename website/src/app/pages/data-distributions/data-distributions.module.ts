7ae4a1b9ea3c8c49f8cf1619a943eb6e0ee20663
axdanbol <daniel_bolin@yahoo.com>
2021-08-17 23:08:13 -0400

    Updated distributions page to handle multiple specs and data filtering

--- website/src/app/pages/data-distributions/data-distributions.module.ts
+++ website/src/app/pages/data-distributions/data-distributions.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxVegaModule } from 'ngx-vega';
 
+import { DatasetSummaryModule } from '../../shared/components/dataset-summary/dataset-summary.module';
 import { LazyVisualizationModule } from '../../shared/components/lazy-visualization/lazy-visualization.module';
 import { TableDataSelectorModule } from '../../shared/components/table-data-selector/table-data-selector.module';
 import { DataDistributionsRoutingModule } from './data-distributions-routing.module';
@@ -11,6 +14,11 @@ import { DataDistributionsComponent } from './data-distributions.component';
   imports: [
     CommonModule,
 
+    MatButtonModule,
+
+    NgxVegaModule,
+
+    DatasetSummaryModule,
     LazyVisualizationModule,
     TableDataSelectorModule,
 
