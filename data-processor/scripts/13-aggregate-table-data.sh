40341fc39b5067f6bb1ec7b2fa6b64aa069cdb79
Edward Lu <eddd.lu@gmail.com>
2024-02-26 14:10:58 -0500

    Remove unwanted text

--- data-processor/scripts/13-aggregate-table-data.sh
+++ data-processor/scripts/13-aggregate-table-data.sh
@@ -3,4 +3,3 @@ source constants.sh
 set -ev
 
 python3 ./src/tables/data.py $DB $SCHEMA $COLUMN_DISTRIBUTION_OVERRIDES -o $AGGREGATE_DATA -d $OUT/site-data -s ../apps/a2agc/src/assets/generated
-starfi
